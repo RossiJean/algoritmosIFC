3e7895c2d3fc468cdc7480ea3fc9b098c8a14313
Jean Rossi <89599205+RossiJean@users.noreply.github.com>
2025-04-22 15:15:12 -0300

    Update 1038_lanche.py

--- Beecrowd/1038_lanche.py
+++ Beecrowd/1038_lanche.py
@@ -1,3 +1,5 @@
+# com match case
+
 a, b = map(int, input().split(" "))
 total = 0
 match a:
@@ -11,4 +13,19 @@ match a:
         total = b * 2.00
     case 5:
         total = b * 1.50
+print(f"Total: R$ {total:.2f}")
+############################################################
+# com if/elif
+a, b = map(int, input().split(" "))
+total = 0
+if a == 1:
+    total = 4.00 * b
+elif a == 2:
+    total = 4.50 * b
+elif a == 3:
+    total = 5.00 * b
+elif a == 4:
+    total = 2.00 * b
+elif a == 5:
+    total = 1.50 * b
 print(f"Total: R$ {total:.2f}")
