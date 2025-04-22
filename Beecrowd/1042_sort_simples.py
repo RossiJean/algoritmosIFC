4cf318b9799bfe8696396fa352c29d43e540161a
Jean Rossi <89599205+RossiJean@users.noreply.github.com>
2025-04-22 16:47:20 -0300

    Create 1042_sort_simples.py

---
+++ Beecrowd/1042_sort_simples.py
@@ -0,0 +1,13 @@
+lista = list(map(int, input().split(" ")))
+lista2 = []
+for i in lista:
+    lista2.append(i)
+lista.sort()
+
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print()
+print(lista2[0])
+print(lista2[1])
+print(lista2[2])
